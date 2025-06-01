96857089c97be53d1fc98667b48a9064d512879e
liht0 <liht0yager@gmail.com>
2025-06-01 21:06:53 +1000

    functions.c

--- functions.c
+++ functions.c
@@ -5,13 +5,13 @@ int sum(int count, ...) {
     va_list args;         
     va_start(args, count); 
     int i;
-    int total = 0;         
+    int result = 0;         
     for(i = 0; i < count; i++) {  
-        total += va_arg(args, int);   
+        result += va_arg(args, int);   
     }
     
     va_end(args);      
-    return total;         
+    return result;         
 }
 
 
@@ -19,16 +19,16 @@ int max(int count, ...) {
     va_list args;
     va_start(args, count);
     int i;
-    int current_max = INT_MIN;  
+    int result_max = INT_MIN;  
     for(i = 0; i < count; i++) {
         int num = va_arg(args, int);  
         if(num > current_max) {      
-            current_max = num;        
+            result_max = num;        
         }
     }
     
     va_end(args);
-    return current_max;
+    return result_max;
 }
 
 
@@ -36,25 +36,25 @@ int min(int count, ...) {
     va_list args;
     va_start(args, count);
     int i;
-    int current_min = INT_MAX;  
+    int result_min = INT_MAX;  
     for(i = 0; i < count; i++) {
         int num = va_arg(args, int); 
-        if(num < current_min) {      
-            current_min = num;      
+        if(num < result_min) {      
+            result_min = num;      
         }
     }
         va_end(args);
-    return current_min;
+    return result_min;
 }
 double sred(int count, ...) {
     va_list args;
     va_start(args, count);
     int i;
-    int total = 0;
+    int result = 0;
     for(i = 0; i < count; i++) {
-        total += va_arg(args, int);  
+        result += va_arg(args, int);  
     }
     
     va_end(args);
-    return (double)total / count;    
+    return (double)result / count;    
 }
